b80f4eca7cbb0d44dfe4d73528a75a2af656a391
Desh Iyer <desh.iyer@gmail.com>
2022-09-07 18:22:30 +0530

    Updated setup files

---
+++ setup/processingJava/processingJava.pde
@@ -0,0 +1,8 @@
+void setup() {
+    size(600, 600);
+    background(51);
+}
+
+void draw() {
+    
+}
---
+++ setup/processingPy/pythonProcessingTest.py
@@ -0,0 +1,6 @@
+def setup():
+    size(200, 200)
+    background(51)
+
+def draw():
+    ellipse(mouseX, mouseY, 10, 10)
